fe3dc47d934f47bcfba5bb13e06b53fca11ae10e
Lia <133962613+Sharinaliaputri@users.noreply.github.com>
2023-06-11 11:31:42 +0700

    Add files via upload

---
+++ Tugas 1.1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main()
+{
+  int a, b, c, d;
+ cout << "isi a";
+ cin >> a;
+ cout << "isi b";
+ cin >> b;
+ cout << "isi c";
+ cin >> c;
+ cout << "isi d";
+ cin >> d;
+ cout << "hasil === " << endl;
+  cout << "Isi variabel a: " << (a + b - c ) << endl;
+  cout << "Isi variabel b: " << a * ((b- c) / d)<< endl;
+
+ 
+  return 0;
+}
+
+
